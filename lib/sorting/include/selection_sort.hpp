67131d6af58875e52afbddf8711314b5d19e27e1
GolomolzinDaniil <golomolzidaniil321@gmail.com>
2026-03-15 23:35:46 +0700

    Selection_step + selection_sort

---
+++ lib/sorting/include/selection_sort.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include <vector>
+
+
+struct Selection_step
+{
+    size_t curr_ind;      // текущий сраниваемый элемент (j)
+    size_t min_index;     // индекс текущего минимума
+    bool is_swap;         // произошла ли на данном этапе смена
+    size_t sorted_num;    // кол-во отсортированных (в начало) (i)
+};
+
+template <typename T>
+std::vector<Selection_step> selection_sort(std::vector<T> &vec)
+{
+    std::vector<Selection_step> history;
+    history.push_back({0,0,false,0});
+
+    const size_t size = vec.size();
+    size_t min_index;
+
+    for (size_t i = 0; i < size - 1; i++)
+    {
+        min_index = i;
+        for (size_t j = i + 1; j < size; j++)
+        {
+            if (vec[j] < vec[min_index]) min_index = j;
+            history.push_back({j,min_index,false,i});
+        }
+        if (min_index != i)
+        {
+            std::swap(vec[i], vec[min_index]);
+            history.push_back({i,min_index,true,i});
+        }
+    }
+    history.push_back({0,0,false,size});
+    return history;
+};
